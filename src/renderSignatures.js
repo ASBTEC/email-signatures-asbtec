d16013d7ea6f05986cbb66820bc1ac9566a86836
AleixMT <aleixaretra@gmail.com>
2025-04-17 14:01:06 +0200

    FIXED: typo

--- src/renderSignatures.js
+++ src/renderSignatures.js
@@ -14,7 +14,7 @@ const template = Handlebars.compile(templateText);
 
 let data = fs.readFileSync(dirPath + "data/user.json", "utf8");
 let json_data = JSON.parse(data)
-let signature = template(json);
+let signature = template(json_data);
 fs.writeFileSync(dirPath + "out/" + json_data.email + ".html", signature, {encoding: "utf8", flag: "w+", mode: 0o666 });
 
 
